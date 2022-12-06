103c26b5a91a04d1fee9de5aab594d3f3f749e8d
jeimar <you@example.com>
2022-12-06 11:58:13 -0500

    modificaciones al juego inicial cambio la apariencia de las tarjetas se agrego audio

--- main.js
+++ main.js
@@ -11,6 +11,14 @@ let timerInicial = 45;
 let time = 45;
 let tiempoRegresivo = null;
 
+//audio
+let winAudio = new Audio('./sounds/sound.wav');
+let loseAudio = new Audio('./sounds/sound.1');
+let clickAudio = new Audio('./sounds/sound.2');
+let rightAudio = new Audio('./sounds/sound.3');
+let wrongAudio = new Audio('./sounds/sound.4');
+
+
 //Apuntando a elemento HTML
 let mostrarMovimientos = document.getElementById('movimientos');
 let mostrarAcietos = document.getElementById('aciertos');
@@ -29,6 +37,7 @@ function contarTiempo(){
     if (time == 0){
       clearInterval(tiempoRegresivo);
       bloquearTarjetas();
+      loseAudio.play();
     }
   },1000)
 } 
@@ -37,7 +46,7 @@ function contarTiempo(){
 function bloquearTarjetas(){
   for (let i = 0; i<=15; i++){
    let tarjetaBloqueada = document.getElementById(i);
-   tarjetaBloqueada.innerHTML = numeros[i];
+   tarjetaBloqueada.innerHTML = `<img src="./imgs/${numeros[i]}.png" alt="">` ;
    tarjetaBloqueada.disabled = true; 
    
   } 
@@ -60,15 +69,15 @@ function destapar(id){
   //mostrar primer numero
    tarjeta1 = document.getElementById(id);
    primerResulatdo = numeros[id]
-   tarjeta1.innerHTML = primerResulatdo;
-
+   tarjeta1.innerHTML = `<img src="./imgs/${primerResulatdo}.png" alt="">`;
+   clickAudio.play();
   //Desabilitar primer boton
   tarjeta1.disabled = true;
   }else if (tarjetasDestapadas ==2){
     //mostra segundo resultado
     tarjeta2 = document.getElementById(id);
     segundoResultado = numeros[id]
-    tarjeta2.innerHTML = segundoResultado;
+    tarjeta2.innerHTML = `<img src="./imgs/${segundoResultado}.png" alt="">`;
 
     //Desabilitar segundo boton
     tarjeta2.disabled = true;
@@ -80,12 +89,12 @@ function destapar(id){
     if (primerResulatdo == segundoResultado) {
       // encender contador tarjetas destapadas
       tarjetasDestapadas = 0;
-
-      //Aumentar aciertos
       aciertos++; 
       mostrarAcietos.innerHTML = `Aciertos: ${aciertos}`;
+      rightAudio.play();
 
       if (aciertos == 8){
+        winAudio.play();
         clearInterval(tiempoRegresivo);
         mostrarAcietos.innerHTML = `Aciertos: ${aciertos} DE 8 has completado la ronda`;
         mostrarTiempo.innerHTML = `Genial has terminado la partida en ${timerInicial - time} segundos `
@@ -93,7 +102,7 @@ function destapar(id){
       }
 
     }else{
-      //mostras momentaneamente valores y volver a tapar
+      wrongAudio.play();
       setTimeout(()=>{
         tarjeta1.innerHTML = ' ';
         tarjeta2.innerHTML = ' ';
